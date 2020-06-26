decba370744df6ed23378bb53b2a3fca75cff844
Ballred95 <blake.allred95@icloud.com>
2020-06-25 23:30:25 -0600

    Initialized entire app.py file for sqlite database

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ import os
 #init app
 app = Flask(__name__)
 
+#create route 
+@app.route('/', methods = ['GET'])
+def get():
+    return jsonify({'msg': 'hello world'})
+
 #run server
 if __name__ == '__main__':
     app.run(debug=True)
