7b0ec9f9b5543e089df3af8c09d957188fb7c43d
Ballred95 <blake.allred95@icloud.com>
2020-06-26 00:12:16 -0600

    added new product to db

--- app.py
+++ app.py
@@ -45,6 +45,17 @@ products_schema = ProductSchema(many=True)
 #Create a Product
 @app.route('/product', methods = ['POST'])
 def add_product():
+    name = request.json['name']
+    description = request.json['description']
+    price = request.json['price']
+    qty = request.json['qty']
+
+    new_product = Product(name, description, price, qty)
+
+    db.session.add(new_product)
+    db.session.commit()
+
+    return product_schema.jsonify(new_product)
 
 #run server
 if __name__ == '__main__':
